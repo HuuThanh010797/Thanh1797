a52fcdd77a06453723b568e67e1353760ea5d13d
HuuThanh010797 <lehuuthanh1797@gmail.com>
2022-09-15 15:59:55 +0700

    thay doi ket noi db

--- web-store/src/main/java/solid/ecom/solidwebstore/config/RootApplicationContextConfig.java
+++ web-store/src/main/java/solid/ecom/solidwebstore/config/RootApplicationContextConfig.java
@@ -1,29 +1,60 @@
 package solid.ecom.solidwebstore.config;
 
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.persistence.SharedCacheMode;
+import javax.persistence.ValidationMode;
 import javax.sql.DataSource;
 
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.core.Ordered;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@ComponentScan("solid.ecom.solidwebstore")
+@EnableTransactionManagement(mode = AdviceMode.PROXY, proxyTargetClass = false, order = Ordered.LOWEST_PRECEDENCE)
+@ComponentScan("solid.ecom.solidwebmanager")
 public class RootApplicationContextConfig {
+	/* Thực thi việc tạo kết nối với Database */
 	@Bean
-	public DataSource dataSource() {
-		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL).addScript("db/sql/create-table.sql")
-				.addScript("db/sql/insert-data.sql").build();
-		return db;
+	public DataSource springJpaDataSource() {
+		JndiDataSourceLookup lookup = new JndiDataSourceLookup();
+		return lookup.getDataSource("jdbc/SpringJpa");
 	}
 
 	@Bean
-	public NamedParameterJdbcTemplate getJdbcTemplate() {
-		return new NamedParameterJdbcTemplate(dataSource());
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+
+		Map<String, Object> properties = new Hashtable<>();
+		properties.put("javax.persistence.schema-generation.database.action", "update");
+		// properties.put("hibernate.hbm2ddl.auto", "update");
+		// properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.format_sql", "true");
+
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabasePlatform("org.hibernate.dialect.PostgreSQLDialect");
+
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setJpaVendorAdapter(adapter);
+		factory.setDataSource(this.springJpaDataSource());
+		factory.setPackagesToScan("solid.ecom.solidwebstore.domain");
+		factory.setSharedCacheMode(SharedCacheMode.ENABLE_SELECTIVE);
+		factory.setValidationMode(ValidationMode.NONE);
+		factory.setJpaPropertyMap(properties);
+		return factory;
+	}
+
+	@Bean
+	public PlatformTransactionManager jpaTransactionManager() {
+		return new JpaTransactionManager(this.entityManagerFactoryBean().getObject());
 	}
 
 }
